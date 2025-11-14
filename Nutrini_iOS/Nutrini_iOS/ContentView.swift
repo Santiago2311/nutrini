fd389552b27b45d1d493c6cfab36eeb3c5963c52
Alumno <lab2203-eic@PUE-EIC-2203-28.local>
2025-11-14 16:54:44 -0600

    Se ve normal

--- Nutrini_iOS/Nutrini_iOS/ContentView.swift
+++ Nutrini_iOS/Nutrini_iOS/ContentView.swift
@@ -9,31 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 60) {
-            
-            ProgressView(value: 0.5)
-                .progressViewStyle(LinearProgressViewStyle(tint: Color(red: 80/255, green: 151/255, blue: 29/255)))
-                .frame(height: 25)
-                .padding(.top, 30)
-                .padding(.horizontal)
+        NavigationStack {
+            VStack(spacing: 60) {
+                
+                ProgressView(value: 0.5)
+                    .progressViewStyle(LinearProgressViewStyle(tint: Color(red: 80/255, green: 151/255, blue: 29/255)))
+                    .frame(height: 25)
+                    .padding(.top, 30)
+                    .padding(.horizontal)
 
-            Image("mascota_icon")
-            
-            HStack {
-                NavigationStack {
+                Image("mascota_icon")
+                
+                HStack {
                     HStack(spacing: 10) {
                         NavigationLink(destination: FoodView()) {
                             MenuButton(iconName: "comida_icon", label: "Comida")
                         }
-                        
+                            
                         NavigationLink(destination: WaterView()) {
                             MenuButton(iconName: "agua_icon", label: "Agua")
                         }
-                        
+                            
                         NavigationLink(destination: ExerciseView()) {
                             MenuButton(iconName: "ejercicio_icon", label: "Ejercicio")
                         }
-                        
+                            
                         NavigationLink(destination: ClosetView()) {
                             MenuButton(iconName: "compras_icon", label: "Closet")
                         }
@@ -42,10 +42,10 @@ struct ContentView: View {
                     .padding(.bottom, 20)
                 }
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(red: 45/255, green: 114/255, blue: 218/255))
+            .ignoresSafeArea()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(red: 45/255, green: 114/255, blue: 218/255))
-        .ignoresSafeArea()
     }
 }
 
